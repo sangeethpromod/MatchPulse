eb7407aa7b98bb7ad339251da9d2b325fd9ffdab
sangeethpromod <sangeethpromod10@gmail.com>
2023-10-25 18:12:52 +0530

    Finished Dashboard

--- client/src/admin/admin.jsx
+++ client/src/admin/admin.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import "./admin.css"
-import Navbar from '../components/navbar';
-import Dashboard from './dashboard';
+import React, { useState } from "react";
+import "./admin.css";
+import Navbar from "../components/navbar";
+import Dashboard from "./dashboard";
+import adminpic from "./admin photos/bekham.jpg";
 
-function admin() {
+function Admin() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleChangePicture = () => {
+    // Define the logic for changing the picture here
+  };
+
+  const handleEditName = () => {
+    // Define the logic for editing the name here
+  };
+
   return (
     <div className="admin-main-container">
       <div className="admin-navbar">
-        <Navbar />
+        <h1>MATCH PULSE</h1>
+        <input
+          className="search-navbar-admin"
+          type="text"
+          placeholder="SEARCH"
+        />
+        <div className="admin-profile-picture">
+          <div className="profile-container" onClick={toggleDropdown}>
+            <img className="adminpic" src={adminpic} alt="User Profile" />
+          </div>
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              <button onClick={handleChangePicture}>Change Picture</button>
+              <button onClick={handleEditName}>Edit Name</button>
+            </div>
+          )}
+          <h1>HENRY KELIN</h1>
+        </div>
       </div>
       <div className="admin-screen-split">
         <div className="admin-sidebar">
@@ -21,11 +53,13 @@ function admin() {
           </div>
         </div>
         <div className="change-content-admin">
-        <div><Dashboard /></div>
+          <div>
+            <Dashboard />
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default admin
+export default Admin;
--- client/src/admin/dashboard.jsx
+++ client/src/admin/dashboard.jsx
@@ -1,18 +1,104 @@
-import React from 'react'
-import "./dashboard.css"
+import React from 'react';
+import "./dashboard.css";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 import dashboardbg from "../images/leauge/6222618.jpg";
-import createPlayer from "./admin photos/Create Player.png"
-import editPlayer from "./admin photos/Edit Player.png"
-import createTeam from "./admin photos/Create Team.png"
+import createPlayer from "./admin photos/Create Player.png";
+import editPlayer from "./admin photos/Edit Player.png";
+import createTeam from "./admin photos/Create Team.png";
 import editTeam from "./admin photos/Edit Team.png";
-import card1giff from "./admin photos/usmt1.png"
-import card2giff from "./admin photos/usmt2.png";
-import card3giff from "./admin photos/usmt3.png";
-import card4giff from "./admin photos/usmt4.png";
 
-import Navbar from '../components/navbar'
 
-function dashboard() {
+function Dashboard() {
+const options = {
+  chart: {
+    type: "line",
+    backgroundColor: "#000000",
+  },
+  title: {
+    text: "Number of Players in Europe's Top 5 Leagues",
+    style: {
+      color: "#ffffff",
+      fontFamily: "DAGGERSQUARE OBLIQUE",
+      letterSpacing: "2px",
+    },
+  },
+  xAxis: {
+    categories: [
+      "Premier League",
+      "La Liga",
+      "Serie A",
+      "Bundesliga",
+      "Ligue 1",
+    ],
+    labels: {
+      style: {
+        color: "#ffffff", // X-axis label text color
+        fontFamily: "DAGGERSQUARE OBLIQUE", // Font family for X-axis labels
+        letterSpacing: "2px",
+      },
+    },
+  },
+  yAxis: {
+    title: {
+      text: "Number of Players",
+      style: {
+        color: "#ffffff",
+        fontFamily: "DAGGERSQUARE OBLIQUE",
+        letterSpacing: "2px",
+      },
+    },
+    labels: {
+      style: {
+        color: "#ffffff", // Y-axis label text color
+        fontFamily: "DAGGERSQUARE OBLIQUE", // Font family for Y-axis labels
+        letterSpacing: "2px",
+      },
+    },
+  },
+  series: [
+    {
+      name: "Players",
+      data: [553, 487, 567, 519, 484],
+      color: "#bd0000",
+      marker: {
+        fillColor: "#ffffff", 
+      },
+    },
+  ],
+};
+
+  const donutOptions = {
+    chart: {
+      type: "pie",
+      backgroundColor: "#000000",
+    },
+    title: {
+      text: "Number of Players",
+      style: {
+        color: "#ffffff",
+        fontFamily: "DAGGERSQUARE OBLIQUE",
+        letterSpacing: "2px",
+      },
+    },
+    series: [
+      {
+        name: "Players",
+        data: [
+          { name: "Number of Existing", y: 2610 }, // Example data for existing players
+          { name: "Number to Be Entered", y: 350 }, // Example data for new players
+        ],
+        colors: ["#bd0000", "#ffffff"], // Colors for the donut chart segments
+        dataLabels: {
+          color: "#ffffff",
+          fontFamily: "DAGGERSQUARE OBLIQUE",
+        },
+      },
+    ],
+  };
+
+
+
   return (
     <div className="dashboard-container-main">
       <div className="dashboard-edit-container">
@@ -46,31 +132,17 @@ function dashboard() {
           </div>
         </div>
         <div className="edit-news-card">
-          <div className="edit-news-card-1">
-            <div className="news-card-1-gif">
-              <img className="news-bg" src={card1giff} />
-            </div>
-            <h1> CLICK TO CHANGE NEWS</h1>
+          <div className="lineGraph">
+            <HighchartsReact highcharts={Highcharts} options={options} />
           </div>
-          <div className="edit-news-card-1">
-            <div className="news-card-1-gif">
-              <img className="dashboard-bg" src={card2giff} />
-            </div>
-          </div>
-          <div className="edit-news-card-1">
-            <div className="news-card-1-gif">
-              <img className="dashboard-bg" src={card3giff} />
-            </div>
-          </div>
-          <div className="edit-news-card-1">
-            <div className="news-card-1-gif">
-              <img className="dashboard-bg" src={card4giff} />
-            </div>
+          <div className="piechart-cont">
+            <HighchartsReact highcharts={Highcharts} options={donutOptions} />
           </div>
         </div>
+        <div className="dashboard-footer"></div>
       </div>
     </div>
   );
 }
 
-export default dashboard
+export default Dashboard
